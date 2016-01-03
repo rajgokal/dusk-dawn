08f7caa11b609db9109c7a0b096098ddafb50fdc
Raj Gokal <rajivgokal@gmail.com>
2016-01-02 16:36:08 -0800

    Initial Commit

---
+++ DuskDawn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DuskDawn
+//
+//  Created by Raj Gokal on 1/2/16.
+//  Copyright © 2016 Raj Gokal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
